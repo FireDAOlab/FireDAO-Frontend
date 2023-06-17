4d6db0e92fc72f2911e072b0a68c67f351d551ba
cityflysky <102033654+Tieroll@users.noreply.github.com>
2023-06-17 11:24:49 +0800

    del

--- src/graph/pools.js
+++ src/graph/pools.js
@@ -54,53 +54,49 @@ export function getFLMPoolData() {
 }
 export function getFDTPoolData() {
     return fetchQueryBase("patton-sr/fdtlockmining", {
-        text: `{
-      adminBackTokenRecords(first: 5) {
-        pid
-        username
-        fid
-        user
-        token
-        amount
-        time
-
-      }
-      adminTransferRecords(first:5) {
-        pid
-        username
-        fid
-        user
-        amount
-        rewardCycle
-        time
-      }
-      depositRecords(first:5) {
-       pid
-       name
-       fid
-       user
-       lpAmount
-        period
-        weightCoefficient
-        time
-      }
-            userClaimFlms(first:5){
-              pid
-              name
-              fid
-              user
-              flmAmount
-              time
-            }
-            extractLpRecords(first:5){
-              pid
-              name
-              fid
-              user
-              lpAmount
-              time
-            }
-
-}`
+        text: `
+        {
+  adminTransferRecords( first:5) {
+  \tid
+ \t\tpid
+  \tusername
+  \tfid
+  \tuser
+  \tamount
+  \trewardCycle
+    time
+\t}
+  depositRecords(first:5) {
+    id 
+    pid
+    name
+    fid
+    user
+    fdtAmount
+    period
+    weightCoefficient
+    time
+  }
+  userClaimFlms(first:5) {
+    id
+    pid
+    name
+    fid
+    user
+    flmAmount
+    time
+  }
+  extractLpRecords(first:5){
+    id
+    pid
+    name
+    fid
+    user
+    fdtAmount
+    time
+}
+}
+        
+        `
     }, "")
 }
